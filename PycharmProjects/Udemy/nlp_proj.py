73c930260480d997af30a6ca7f1e72299dcf2482
srijan-das <srijan.das@hotmail.com>
2020-02-29 20:07:23 +0530

    Fingers Crossed

---
+++ PycharmProjects/Udemy/nlp_proj.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+yelp = pd.read_csv("/home/srijan/PycharmProjects/mlaicrc/PycharmProjects/Py-DS-ML-Bootcamp-master/Refactored_Py_DS_ML_Bootcamp-master/20-Natural-Language-Processing/yelp.csv")
+'''
+print(yelp.head())
+print(yelp.describe())
+print(yelp.info())
+print(yelp.columns)
+'''
